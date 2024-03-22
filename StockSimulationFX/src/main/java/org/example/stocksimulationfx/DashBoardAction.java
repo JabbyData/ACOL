48468e7dedddf3785d0d7a4983c019f08d353814
antonin <antonin.point@grenoble-inp.org>
2024-03-22 14:59:15 +0100

    quit + deco for modeAction

--- StockSimulationFX/src/main/java/org/example/stocksimulationfx/DashBoardAction.java
+++ StockSimulationFX/src/main/java/org/example/stocksimulationfx/DashBoardAction.java
@@ -14,8 +14,14 @@ public class DashBoardAction extends Application {
     public void start(Stage stage) throws Exception {
         dashboardWindow = stage;
         javafx.fxml.FXMLLoader fxmlLoader = new javafx.fxml.FXMLLoader(DashBoardAction.class.getResource("dashboard-modeAction.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setScene(scene);
+        stage.setOnCloseRequest(e -> {
+            e.consume();
+            boolean answer = ConfirmQuitBox.display("Confirm Quit", "Are you sure you want to quit?");
+            if (answer)
+                stage.close();
+        });
         stage.show();
     }
 }
--- StockSimulationFX/src/main/java/org/example/stocksimulationfx/DashboardActionController.java
+++ StockSimulationFX/src/main/java/org/example/stocksimulationfx/DashboardActionController.java
@@ -1,4 +1,31 @@
 package org.example.stocksimulationfx;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DashboardActionController {
+    @FXML
+    private TextField Date;
+
+    @FXML
+    void handleQuit(){
+        Stage stage = (Stage) Date.getScene().getWindow();
+        boolean answer = ConfirmQuitBox.display("Confirm Quit", "Are you sure you want to quit?");
+        if (answer)
+            stage.close();
+    }
+
+    @FXML
+    void handleDeconnexion() throws IOException {
+        Stage stage = (Stage) Date.getScene().getWindow();
+        boolean answer = ConfirmQuitBox.display("Confirm Decconexion", "Are you sure you want to deconnect?");
+        if (answer) {
+            stage.close();
+            LoginApplication loginApplication = new LoginApplication();
+            loginApplication.start(new Stage());
+        }
+    }
 }
